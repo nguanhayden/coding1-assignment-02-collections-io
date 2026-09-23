dda3b85c95587f49d0d07839fab027195ea51ea0
Hayden Nguan <nguanhayden@Haydens-MacBook-Pro.local>
2026-09-23 17:36:57 +0200

    changes:

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" #print will be tea
+sales = {"tea": 3, "coffee": 5} #print will be 3
+sold = sales[drink] #print will be 3
+sold = sold + 1 #print will be 4
+print(drink) #print will be tea
+print(sold) #print will be 4
